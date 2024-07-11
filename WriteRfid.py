c9c86451e878db6b9ce8e0ed825e4311278f00ec
@felix.puck <felix.puck@telekom.de>
2024-07-12 00:31:49 +0200

    update

--- WriteRfid.py
+++ WriteRfid.py
@@ -1,4 +1,4 @@
-from smbus import SMBus
+from smbus2 import SMBus, i2c_msg
 import time
 
 bus = SMBus(1)
@@ -7,16 +7,22 @@ slave_address = 0x08  # Feste I2C-Slave-Adresse des Arduino
 
 def write_rfid(block_addr, data):
     try:
+        # Befehl (0x02) und Blockadresse an den Anfang der Nachricht setzen
         byte_data = [0x02, block_addr] + [ord(c) for c in data.ljust(16, '\0')]
         print(f"Sende Daten: {byte_data}")  # Debug-Ausgabe der gesendeten Daten
-        bus.write_i2c_block_data(slave_address, 0, byte_data)
+
+        # Sende Daten an den Arduino
+        write = i2c_msg.write(slave_address, byte_data)
+        bus.i2c_rdwr(write)
         print(f"Daten gesendet: {data}")
 
         # Warten Sie eine Weile, um dem Arduino Zeit zu geben, den Tag zu erkennen und zu schreiben
         time.sleep(1)
 
         # Lesen Sie die Antwort vom Arduino
-        response = bus.read_i2c_block_data(slave_address, 0, 16)
+        read = i2c_msg.read(slave_address, 16)
+        bus.i2c_rdwr(read)
+        response = list(read)
         response_str = ''.join(chr(i) for i in response if i != 0)
         print(f"Antwort vom Arduino: {response_str}")
 
